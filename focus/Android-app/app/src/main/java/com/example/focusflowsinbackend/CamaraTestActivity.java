93c6dc850e1470c9a3eb0d5c38589ef219c87184
irllymissher <tomascalderon@proton.me>
2026-04-29 11:03:32 +0200

    Se añade una clase mock para probar la camara

---
+++ focus/Android-app/app/src/main/java/com/example/focusflowsinbackend/CamaraTestActivity.java
@@ -0,0 +1,18 @@
+package com.example.focusflowsinbackend;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CamaraTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara_test);
+    }
+}
